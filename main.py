f9b53bdf835e7a9014bfbc2ebe54b32037c8bee6
Sergio <garci01020304@gmail.com>
2026-04-20 14:37:18 +0200

    "Style: Corrección de estilo PEP 8 y code smells con Ruff"

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from personajes import Jugador,Aldeano,lobo,Vidente
-from logica import VICTORIAALDEANOS,VICTORIALOBOS,gestorPartida
+from logica import gestorPartida
 
-# if __name__ == "__main__":
-juego=gestorPartida()
-juego.anadirJugador("Nacho", "lobo")
-juego.anadirJugador("Elena", "vidente")
-juego.anadirJugador("Carlos","aldeano")
-print(juego.jugadores[0].AccionNocturna(juego.jugadores[2]))
-print(juego.ComprobarVictoria())    
+if __name__ == "__main__":
+    juego=gestorPartida()
+    juego.anadirJugador("Nacho", "lobo")
+    juego.anadirJugador("Elena", "vidente")
+    juego.anadirJugador("Carlos","aldeano")
+    print(juego.jugadores[0].AccionNocturna(juego.jugadores[2]))
+    print(juego.ComprobarVictoria())    
